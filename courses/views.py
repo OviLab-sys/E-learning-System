61316b35585d2548733a6ef875bc9973a6bc5702
vicoduorr <victoroduorr@gmail.com>
2023-11-30 14:03:30 +0300

    i go to space with the starz

--- courses/views.py
+++ courses/views.py
@@ -12,6 +12,7 @@ from .forms import ModuleFormSet
 from django.forms.models import modelform_factory
 from django.apps import apps
 from .models import Module, Content
+from braces.views import CsrfExemptMixin, JsonRequestResponseMixin
 
 
 class OwnerMixin:
@@ -144,3 +145,15 @@ class ModuleContentListView(TemplateResponseMixin, View):
         module = get_object_or_404(Module,id=module_id,
                                    course__owner=request.user)
         return self.render_to_response({'module':module})
+    
+class ModuleOrderView(CsrfExemptMixin, JsonRequestResponseMixin, View):
+    def post(self,request):
+        for id, order in self.request__json.items():
+            Module.objects.filter(id=id,course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved':'OK'})
+
+class ContentOrderView(CsrfExemptMixin, JsonRequestResponseMixin,View):
+    def post(self,request):
+        for id, order in self.request_json.items():
+            Content.objects.filter(id=id,module__course__owner=request.user).update(order=order)
+        return self.render_json_response({'saved':'OK'})
